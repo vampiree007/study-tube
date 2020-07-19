d590ff5dfb135b6997261cafbfca166f8a7b6331
vampiree007 <vikrantgupta.psit@gmail.com>
2020-07-19 17:05:05 +0530

    vhnaging proxy setup to heroku pandatube do change to local 5000 before use.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware')
 const createProxyMiddleware = proxy.createProxyMiddleware
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" ,
+    app.use(createProxyMiddleware("/api", { target: "http://pandatube.herokuapp.com/" ,
     changeOrigin: true}));
 };
