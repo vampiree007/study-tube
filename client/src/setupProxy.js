bdfe6bb0b302ff28630e257b31d8c1c97dfd2f61
vampiree007 <vikrantgupta.psit@gmail.com>
2020-07-19 18:52:37 +0530

    trying again to deploy on heroku

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware')
 const createProxyMiddleware = proxy.createProxyMiddleware
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware("/api", { target: "http://pandatube.herokuapp.com/" ,
+    app.use(createProxyMiddleware("/api", { target: "https://pandatube.herokuapp.com" ,
     changeOrigin: true}));
 };
